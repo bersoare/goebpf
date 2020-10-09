6f8f3880a45c0187d3df4a8657c440460ad4e1eb
bersoare <bsoares.it@gmail.com>
2020-10-09 12:31:10 -0300

    using bpf_prog_test_run_xattr instead

--- progtest.go
+++ progtest.go
@@ -20,28 +20,69 @@ package goebpf
 #define SYSCALL_BPF(command)		0
 #endif
 
-int bpf_prog_test_run(int prog_fd, int repeat, void *data, __u32 size,
-		      void *data_out, __u32 *size_out, __u32 *retval,
-		      __u32 *duration)
+// struct used to run the test
+// https://elixir.bootlin.com/linux/latest/source/tools/lib/bpf/bpf.h#L192
+struct bpf_prog_test_run_attr {
+	int prog_fd;
+	int repeat;
+	const void *data_in;
+	__u32 data_size_in;
+	void *data_out;
+	__u32 data_size_out;
+	__u32 retval;
+	__u32 duration;
+	const void *ctx_in;
+	__u32 ctx_size_in;
+	void *ctx_out;
+	__u32 ctx_size_out;
+};
+
+int bpf_prog_test_run_xattr(struct bpf_prog_test_run_attr *test_attr)
 {
 	union bpf_attr attr;
 	int ret;
 
+	if (!test_attr->data_out && test_attr->data_size_out > 0)
+		return -EINVAL;
+
 	memset(&attr, 0, sizeof(attr));
-	attr.test.prog_fd = prog_fd;
-	attr.test.data_in = ptr_to_u64(data);
-	attr.test.data_out = ptr_to_u64(data_out);
-	attr.test.data_size_in = size;
-	attr.test.repeat = repeat;
+	attr.test.prog_fd = test_attr->prog_fd;
+	attr.test.data_in = ptr_to_u64(test_attr->data_in);
+	attr.test.data_out = ptr_to_u64(test_attr->data_out);
+	attr.test.data_size_in = test_attr->data_size_in;
+	attr.test.data_size_out = test_attr->data_size_out;
+	attr.test.ctx_in = ptr_to_u64(test_attr->ctx_in);
+	attr.test.ctx_out = ptr_to_u64(test_attr->ctx_out);
+	attr.test.ctx_size_in = test_attr->ctx_size_in;
+	attr.test.ctx_size_out = test_attr->ctx_size_out;
+	attr.test.repeat = test_attr->repeat;
 
 	ret = SYSCALL_BPF(BPF_PROG_TEST_RUN);
-	if (size_out)
-		*size_out = attr.test.data_size_out;
-	if (retval)
-		*retval = attr.test.retval;
-	if (duration)
-		*duration = attr.test.duration;
+	test_attr->data_size_out = attr.test.data_size_out;
+	test_attr->ctx_size_out = attr.test.ctx_size_out;
+	test_attr->retval = attr.test.retval;
+	test_attr->duration = attr.test.duration;
 	return ret;
+}
+
+int ebpf_prog_test_run(int prog_fd, int repeat, void *data,
+		      void *data_out, __u32 *size_out, __u32 *retval,
+			  __u32 *duration)
+{
+	int ret;
+
+	struct bpf_prog_test_run_attr test_attr = {
+			.prog_fd	= prog_fd,
+			.repeat 	= repeat,
+			.data_in	= data,
+			.data_out	= data_out,
+	};
+	ret = bpf_prog_test_run_xattr(&test_attr);
+	*size_out = test_attr.data_size_out;
+	*retval = test_attr.retval;
+	*duration = test_attr.duration;
+	return ret;
+
 };
 
 */
@@ -64,15 +105,18 @@ type ProgTest struct {
 // docstring
 func (p *ProgTest) progTestRun() error {
 	var dur C.uint
+	defer C.free(unsafe.Pointer(&dur))
 	var retval C.uint
+	defer C.free(unsafe.Pointer(&retval))
 	var size_out C.uint
+	defer C.free(unsafe.Pointer(&size_out))
 
-	ret := C.bpf_prog_test_run(C.int(p.ProgFD), C.int(p.Repeat),
-		unsafe.Pointer(&p.InData[0]), C.__u32(len(p.InData)),
+	ret := C.ebpf_prog_test_run(C.int(p.ProgFD), C.int(p.Repeat),
+		unsafe.Pointer(&p.InData[0]),
 		unsafe.Pointer(&p.OutData[0]), &size_out,
 		&retval, &dur)
 	if ret == -1 {
-		return fmt.Errorf("bpf_prog_test_run() failed\n")
+		return fmt.Errorf("ebpf_prog_test_run() failed\n")
 	}
 	p.Retval = int(retval)
 	p.Duration = int(dur)
